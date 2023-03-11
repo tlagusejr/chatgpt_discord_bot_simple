48d1bc5a3ef2a9e887e3f24efc77c7c1dd599a15
심현덕 <52907198+tlagusejr@users.noreply.github.com>
2023-03-11 15:36:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,12 @@ import sys
 
 # 시스템 인코딩 설정의 출력
 
-openai.api_key = "오픈ai api 코드"
+openai.api_key = "오픈ai api 키"
 
 # 모델 - GPT 3.5 Turbo 선택
 model = "gpt-3.5-turbo"
 
-messages_json=[
-            {},
-        ]
+messages_json=[]
 bot = commands.Bot(command_prefix='/')
  
 @bot.event
@@ -22,30 +20,31 @@ async def on_ready():
     print(f'Login bot: {bot.user}')
 @bot.event
 async def on_message(message):
+    global messages_json
     if message.author == bot.user:
         return
     
     query = message.content
-    messages_json.append({"role":"user","content":query})
-    response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-            {"role": "user", "content": query},
-        ]
-    )
-    answer = response['choices'][0]['message']['content']
-    print(answer)
-    messages_json.append({"role":"assistant","content":answer})
-    await message.author.send(answer)
+    query = query[1:]
+    if query == "대화종료":
+        messages_json=[]
+        print("대화가 종료되었습니다.")
+        
+        await message.author.send("대화 종료")
+    else:
 
-@bot.command()
-async def hello(message):
-    await message.channel.send('Hi!')
-@bot.command()
-async def 대화종료(message):
-    messages_json=[
-            {},
-        ]
-    await message.channel.send("대화가 종료되었습니다.")
+        messages_json.append({"role":"user","content":query})
+        response = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+                {"role": "user", "content": query},
+            ]
+        )
+        answer = response['choices'][0]['message']['content']
+        print(answer)
+        print(messages_json)
+        messages_json.append({"role":"assistant","content":answer})
+        await message.author.send(answer)
+
  
-bot.run('챗봇 코드')
+bot.run('')
